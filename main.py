a241510e35a312ecd701c64d10d6bc88d72e0117
Kiryu <portagate6@gmail.com>
2023-06-19 13:17:29 +1200

    19/06/2023 finished the layout of the main menu

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tkinter
 import customtkinter
 
+def transition():
+    splash_screen.pack_forget()
+    home_screen.pack()
 
+def label(text, frame):
+    z = customtkinter.CTkLabel(frame, text=text, font=primary)
+    return z
 
+def f_button(text, frame, command):
+    z = customtkinter.CTkButton(frame, text=text, command=command)
+    return z
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
@@ -19,15 +28,28 @@ secondary = customtkinter.CTkFont(family='SF Pro Display', size=14)
 
 #--------Splash Screen-------#
 splash_screen = customtkinter.CTkFrame(window)
-splash = customtkinter.CTkLabel(window, text='Welcome', font=primary)
+splash_screen.pack()
+splash = customtkinter.CTkLabel(splash_screen, text='Welcome', font=primary)
 splash.pack(pady=10)
 
-button = customtkinter.CTkButton(window, text='Get Started', font=secondary)
+button = customtkinter.CTkButton(splash_screen, text='Get Started', font=secondary, command=transition)
 button.pack(pady=10)
 
 #-------Home Screen--------#
 home_screen = customtkinter.CTkFrame(window)
+home_label = customtkinter.CTkLabel(home_screen, text='Main Menu', font=primary)
+home_label.pack()
 
+study_button = customtkinter.CTkButton(home_screen, text="Study", font=primary)
+study_button.pack(pady=10)
 
+options_button = customtkinter.CTkButton(home_screen, text="Options", font=primary)
+options_button.pack(pady=10)
+
+help_button = customtkinter.CTkButton(home_screen, text="Help", font=primary)
+help_button.pack(pady=10)
+
+suit_button = customtkinter.CTkButton(home_screen, text="Quit", font=primary, command=quit)
+suit_button.pack(pady=10)
 
 window.mainloop()
